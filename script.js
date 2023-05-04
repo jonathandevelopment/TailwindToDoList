d2fd465b78890832023d047c90290bf16a6016d3
Jonathancodesurfer <jonathan@ivcmexico.com>
2023-05-04 15:41:07 -0600

    update

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-//document.getElementById("button").addEventListener("click", addTask);
-
-// var counter = 0;
-// function addTask() {
-//   counter++;
-//   let task = document.getElementById("task").value;
-//   let list = document.getElementById("to-do-list");
-
-//   list.innerHTML += 
-//   `<li id='${counter}'> ${task} 
-//     <button style="color:red" id="btn${counter}" 
-//     onclick="deleteTask(${counter})"> 
-//     DELETE TASK
-//     </button>
-//   </li>`;
-// }
-// function deleteTask(id) {
-//   console.log(id);
-//   document.getElementById(id.toString()).remove();
-// }
 
 var counter = 0;
 function addTask() {
